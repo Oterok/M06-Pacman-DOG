b4459085cb7eef7addc88aa9d5ff860dc89b7602
David Otero <doterog@gmail.com>
2018-11-14 19:05:36 +0100

    Solucionado el problema con la iteración que hacia que se llamase de forma exponencial el metodo refrescar(). Agregado el metodo movValido() que es un tipo de movimiento muy basico que puede llevar a busces a los fantasmas (sirve apra realizar pruebas concretas).

--- public_html/Tablero.js
+++ public_html/Tablero.js
@@ -17,9 +17,10 @@ generarJugador();
 generarFantasma();
 //comprobarDatosFantasma(); //Variable que imprime con alert la inforamcion de los fantasmas.
 
-imprimirTablero(); //Impimimos por primera vez el tablero.
+//imprimirTablero(); //Impimimos por primera vez el tablero.
+imprimirInformacion();
 
-refrescar()//Este metodo entrara en bucle hasta que se acabe el juego.
+refrescar();//Este metodo entrara en bucle hasta que se acabe el juego.
 
 //---------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------TABLERO------------------------------------------------------------------------
@@ -72,7 +73,6 @@ function generarTablero() {
 //---------------------------------------------------------------------------------------------------------------------------
 
 
-
 function generarJugador() {
     var pos = posicionValida();
     jugador.set("X", pos[0]);
@@ -95,6 +95,7 @@ function comprobarDatosJugador() {
 
 function movimientoFantasma() {
     for (var i = 0; i != cantidadFantasmas; i++) {
+        //var variables = movValido(grupoFantasmas[i].get("X"), grupoFantasmas[i].get("Y"), grupoFantasmas[i].get("Dir"));
         var variables = movimientoValido(grupoFantasmas[i].get("X"), grupoFantasmas[i].get("Y"), grupoFantasmas[i].get("Dir"));
         grupoFantasmas[i].set("X", variables[0]);
         grupoFantasmas[i].set("Y", variables[1]);
@@ -124,15 +125,42 @@ function comprobarDatosFantasma() {
 }
 
 //---------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------VARIABLES COMUNAS--------------------------------------------------------------
+//--------------------------------------------FUNC. COMUNAS------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------------------------------
 
 function refrescar() {
     document.body.innerHTML = "";
     movimientoFantasma();
-    //comprobarDatosFantasma();
     imprimirTablero();
-    setInterval('refrescar()', 100);
+    imprimirInformacion();
+}
+
+function movValido(posiX, posiY, dir) {
+    var direccionValida = [0, 0, 0, 0];
+    var variables = [0, 0, 0];
+
+    if (comprobarY(posiY) && tablero[posiX][posiY - 1] == 1) {
+        variables[0] = posiX;
+        variables[1] = posiY - 1;
+        variables[2] = 2; //direcció
+    }
+    if (comprobarCoordMinima(posiY) && tablero[posiX][posiY + 1] == 1) {
+        variables[0] = posiX;
+        variables[1] = posiY + 1;
+        variables[2] = 3;
+    }
+    if (comprobarCoordMinima(posiX) && tablero[posiX - 1][posiY] == 1) {
+        variables[0] = posiX - 1;
+        variables[1] = posiY;
+        variables[2] = 1;
+    }
+    if (comprobarX(posiX) && tablero[posiX + 1][posiY] == 1) {
+        variables[0] = posiX + 1;
+        variables[1] = posiY;
+        variables[2] = 0;
+    }
+    //alert("posx: "+posiX+" / posy: "+posiY+" / dir: "+variables[2]);
+    return variables;
 }
 
 function movimientoValido(posiX, posiY, dir) {
@@ -140,55 +168,51 @@ function movimientoValido(posiX, posiY, dir) {
     //0=Up(y+1), 1=Down(y-1), 2=Left(x-1), 3=Right(x+1), 4=Stop
     var direccionValida = [0, 0, 0, 0, 0];
     var variables = [0, 0, 0]; //Esta variable devolvera la X, la Y y la dir.
-    //El switch comprueva donde se podra mover en funcion de la dirección que se tenia, 
+    //El switch comprueba donde se podra mover en funcion de la dirección que se tenia, 
     //no permite volver atras para evitar que un fantasma se quede siempre en dos casillas dando vueltas.
     switch (dir) {
         case 0:
-            //alert("0");
-            if (tablero[posiX][posiY + 1] == 1) {
-                direccionValida[0] = 1
+            if (comprobarX(posiX) && tablero[posiX + 1][posiY] == 1) {
+                direccionValida[0] = 1;
             }
-            if (tablero[posiX - 1][posiY] == 1) {
-                direccionValida[2] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY - 1] == 1) {
+                direccionValida[2] = 1;
             }
-            if (tablero[posiX + 1][posiY] == 1) {
-                direccionValida[3] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY + 1] == 1) {
+                direccionValida[3] = 1;
             }
             break;
         case 1:
-            //alert("1");
-            if (tablero[posiX][posiY - 1] == 1) {
-                direccionValida[1] = 1
+            if (comprobarX(posiX) && tablero[posiX - 1][posiY] == 1) {
+                direccionValida[1] = 1;
             }
-            if (tablero[posiX - 1][posiY] == 1) {
-                direccionValida[2] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY - 1] == 1) {
+                direccionValida[2] = 1;
             }
-            if (tablero[posiX + 1][posiY] == 1) {
-                direccionValida[3] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY + 1] == 1) {
+                direccionValida[3] = 1;
             }
             break;
         case 2:
-            //alert("2");
-            if (tablero[posiX][posiY + 1] == 1) {
-                direccionValida[0] = 1
+            if (comprobarX(posiX) && tablero[posiX + 1][posiY] == 1) {
+                direccionValida[0] = 1;
             }
-            if (tablero[posiX][posiY - 1] == 1) {
-                direccionValida[1] = 1
+            if (comprobarX(posiX) && tablero[posiX - 1][posiY] == 1) {
+                direccionValida[1] = 1;
             }
-            if (tablero[posiX - 1][posiY] == 1) {
-                direccionValida[2] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY - 1] == 1) {
+                direccionValida[2] = 1;
             }
             break;
         case 3:
-            //alert("3");
-            if (tablero[posiX][posiY + 1] == 1) {
-                direccionValida[0] = 1
+            if (comprobarX(posiX) && tablero[posiX + 1][posiY] == 1) {
+                direccionValida[0] = 1;
             }
-            if (tablero[posiX][posiY - 1] == 1) {
-                direccionValida[1] = 1
+            if (comprobarX(posiX) && tablero[posiX - 1][posiY] == 1) {
+                direccionValida[1] = 1;
             }
-            if (tablero[posiX + 1][posiY] == 1) {
-                direccionValida[3] = 1
+            if (comprobarY(posiY) && tablero[posiX][posiY + 1] == 1) {
+                direccionValida[3] = 1;
             }
             break;
     }
@@ -199,28 +223,28 @@ function movimientoValido(posiX, posiY, dir) {
         dir = Math.floor(Math.random() * 4);
     }
 
-    variables[2] = dir; //Contien la dirección del fantasma.
+    variables[2] = dir; //Contiene la dirección del fantasma.
     switch (dir)
     {
         case 0:
             //alert("Up");
-            variables[0] = posiX; //Contien la X del fantasma.
-            variables[1] = posiY + 1; //Contien la Y del fantasma.
+            variables[0] = posiX + 1; //Contiene la X del fantasma.
+            variables[1] = posiY; //Contiene la Y del fantasma.
             break;
         case 1:
             //alert("Down");
-            variables[0] = posiX; //Contien la X del fantasma.
-            variables[1] = posiY - 1; //Contien la Y del fantasma.
+            variables[0] = posiX - 1; //Contiene la X del fantasma.
+            variables[1] = posiY; //Contiene la Y del fantasma.
             break;
         case 2:
             //alert("Left");
-            variables[0] = posiX - 1; //Contien la X del fantasma.
-            variables[1] = posiY; //Contien la Y del fantasma.
+            variables[0] = posiX; //Contiene la X del fantasma.
+            variables[1] = posiY - 1; //Contiene la Y del fantasma.
             break;
         case 3:
             //alert("Right");
-            variables[0] = posiX; //Contien la X del fantasma.
-            variables[1] = posiY + 1; //Contien la Y del fantasma.
+            variables[0] = posiX; //Contiene la X del fantasma.
+            variables[1] = posiY + 1; //Contiene la Y del fantasma.
             break;
     }
     return variables;
@@ -231,16 +255,16 @@ function direccionInicialValida(posiX, posiY) {
     var direccionValida = [0, 0, 0, 0, 0];
     var count = 5;
     if (tablero[posiX][posiY + 1] == 1) {
-        direccionValida[0] = 1
+        direccionValida[0] = 1;
     }
     if (tablero[posiX][posiY - 1] == 1) {
-        direccionValida[1] = 1
+        direccionValida[1] = 1;
     }
     if (tablero[posiX - 1][posiY] == 1) {
-        direccionValida[2] = 1
+        direccionValida[2] = 1;
     }
     if (tablero[posiX + 1][posiY] == 1) {
-        direccionValida[3] = 1
+        direccionValida[3] = 1;
     }
 
     while (direccionValida[count] != 0) {
@@ -263,21 +287,21 @@ function posicionValida() {
 
 function imprimirTablero() {
 //    El siguinete codigo comentado sirve para impimir una ayuda para saber la posicion Y del tablero.
-//    document.write("Y:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
-//    for (var x = 0; x <= 3 - 1; x++) {
-//        for (var y = 0; y <= 10 - 1; y++) {
-//            document.write(y + "&nbsp");
-//        }
-//    }
-//document.write("</br>");
+    document.write("Y:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
+    for (var x = 0; x <= 3 - 1; x++) {
+        for (var y = 0; y <= 10 - 1; y++) {
+            document.write(y + "&nbsp");
+        }
+    }
+    document.write("</br>");
 
     for (var x = 0; x <= tablero.length - 1; x++) {
 //    El siguinete codigo comentado sirve para impimir una ayuda para saber la posicion X del tablero.
-//        if (x < 10) {
-//            document.write("X:0" + x + ":&nbsp&nbsp&nbsp&nbsp");
-//        } else {
-//            document.write("X:" + x + ":&nbsp&nbsp&nbsp&nbsp");
-//        }
+        if (x < 10) {
+            document.write("X:0" + x + ":&nbsp&nbsp&nbsp&nbsp");
+        } else {
+            document.write("X:" + x + ":&nbsp&nbsp&nbsp&nbsp");
+        }
         for (var y = 0; y <= tablero[0].length - 1; y++) {
             if (jugador.get("X") == x && jugador.get("Y") == y) {
                 document.write("J" + "&nbsp");
@@ -303,3 +327,42 @@ function imprimirTablero() {
         document.write("</br>");
     }
 }
+
+function imprimirInformacion() {
+    for (var i = 0; i != cantidadFantasmas; i++) {
+        document.write("Fantasma" + i + " X: " + grupoFantasmas[i].get("X") + " - Y: " + grupoFantasmas[i].get("Y") + " - Dir: " + grupoFantasmas[i].get("Dir"));
+        document.write("</br>");
+    }
+}
+
+function comprobarY(posiY) {
+    var bool;
+    if (posiY <= tablero[0].length - 1) {
+        bool = true;
+    } else {
+        bool = false;
+    }
+    return bool;
+}
+
+function comprobarX(posiX) {
+    var bool;
+    if (posiX <= tablero.length - 1) {
+        bool = true;
+    } else {
+        bool = false;
+    }
+    return bool;
+}
+
+function comprobarCoordMinima(posiY) {
+    var bool;
+    if (posiY >= 0) {
+        bool = true;
+    } else {
+        bool = false;
+    }
+    return bool;
+}
+
+setInterval(refrescar, 1000);
